43012e327c895a35783496903dcce48aa1fb7cb2
Dagaet <alojera15@gmail.com>
2024-02-18 18:30:35 +0000

    feat(): added main menu with simple scene

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,17 @@
+class_name MainMenu
+extends Control
+
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/ExitButton as Button
+
+@export var start_level = preload("res://Scenes/MainMenu/main_scene.tscn") as PackedScene
+
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+
+func on_exit_pressed() -> void:
+	get_tree().quit()

2f431ddb73cfd722a78ff6305f5c89d64fd0c0f8
Dagaet <alojera15@gmail.com>
2024-02-21 15:40:28 +0000

    feat(): enemy movement system

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,16 +1,28 @@
 extends CharacterBody2D
 @onready var _animated_enemy_sprite = $AnimatedSprite2D
 
-var velocity1 = Vector2(100,0)
-func _physics_process(delta):
-	_animated_enemy_sprite.play("idle")
-	#velocity1.y += 7
-	
-	#if is_on_wall():
-		#velocity1.x *= -1
-		#$AnimatedSprite2D.flip_h = ! $AnimatedSprite2D.flip_h
+var speed = -25
+var facing_right = false
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(_delta):
+	_animated_enemy_sprite.play("idle")
+	if not is_on_floor():
+		velocity.y += gravity * _delta
+		
+	if !$RayCast2D.is_colliding() && is_on_floor():
+		flip()
+	velocity.x = speed
 	move_and_slide()
+
+func flip():
+	facing_right = !facing_right
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		speed = abs(speed)
+	else:
+		speed = abs(speed) * -1
 
 func _on_top_body_entered(body):
 	if body.get_name() == "Player":
--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,41 +1,29 @@
 extends CharacterBody2D
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -800.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -450.0
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var customGravity = 1000
 
 
 func _physics_process(delta):
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity.y += customGravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * SPEED
 	
 	if direction != 0:
 		_animated_sprite.flip_h = (direction == -1)
 	update_animations(direction)
-	#if direction:
-	#	if velocity.x > 0:
-	#		_animated_sprite.flip_h = false 
-	#	elif velocity.x < 0:
-	#		_animated_sprite.flip_h = true
-	#	_animated_sprite.play("walk")
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
-	#	_animated_sprite.play("idle")
+
 	move_and_slide()
 func update_animations(direction):
 	if is_on_floor():

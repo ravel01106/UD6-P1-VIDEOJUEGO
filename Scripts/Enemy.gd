52a50e1c3e78353db566761e5362923d9ed4f1fd
Dagaet <alojera15@gmail.com>
2024-02-21 13:58:24 +0000

    feat(): implemented killing mobs when landing on them

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+@onready var _animated_enemy_sprite = $AnimatedSprite2D
+
+var velocity1 = Vector2(100,0)
+func _physics_process(delta):
+	_animated_enemy_sprite.play("idle")
+	#velocity1.y += 7
+	
+	#if is_on_wall():
+		#velocity1.x *= -1
+		#$AnimatedSprite2D.flip_h = ! $AnimatedSprite2D.flip_h
+
+	move_and_slide()
+
+func _on_top_body_entered(body):
+	if body.get_name() == "Player":
+		queue_free()
+
+func _on_sides_body_entered(body):
+	pass # Replace with function body.

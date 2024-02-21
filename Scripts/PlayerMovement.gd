c6ef935601545dbb8f430c4ac7d8ca3bd4e60de0
Dagaet <alojera15@gmail.com>
2024-02-21 09:50:44 +0000

    feat(): implemented animations to the player

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,17 +1,19 @@
 extends CharacterBody2D
 
+const SPEED = 600.0
+const JUMP_VELOCITY = -800.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var _animated_sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var customGravity = 1000
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += customGravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -20,9 +22,29 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity.x = direction * SPEED
+	
+	if direction != 0:
+		_animated_sprite.flip_h = (direction == -1)
+	update_animations(direction)
+	#if direction:
+	#	if velocity.x > 0:
+	#		_animated_sprite.flip_h = false 
+	#	elif velocity.x < 0:
+	#		_animated_sprite.flip_h = true
+	#	_animated_sprite.play("walk")
+	#else:
+	#	velocity.x = move_toward(velocity.x, 0, SPEED)
+	#	_animated_sprite.play("idle")
 	move_and_slide()
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			_animated_sprite.play("idle")
+		else :
+			_animated_sprite.play("walk")
+	else:
+		if velocity.y < 0:
+			_animated_sprite.play("jump") 
+		elif velocity.y > 0:
+			_animated_sprite.play("fall")

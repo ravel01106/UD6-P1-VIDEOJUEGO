e8838284be374ebcbf70d75121b2607851f0d3cc
ravel01106 <amandaravelocabrera@gmail.com>
2024-03-01 10:39:10 +0000

    fix(): fixed end and mainScene

--- Scripts/EndScene.gd
+++ Scripts/EndScene.gd
@@ -1,14 +1,19 @@
 extends Control
 @export var start_level = preload("res://Scenes/MainMenu/main_scene.tscn") as PackedScene
-@onready var _label_counter = $MarginContainer/VBoxContainer/Info
+@onready var _label_counter = $MarginContainer/VBoxContainer/VBoxContainer/Info
+
+@onready var restart_button = $MarginContainer/HBoxContainer/VBoxContainer/Restart as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit as Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$EndSceneAudio.play()
+	restart_button.button_down.connect(_on_restart_pressed)
+	exit_button.button_down.connect(_on_exit_pressed)
 	_label_counter.text = "Enemies killed: " + str(Global.enemyKilledCount)
 
-func _on_start_button_pressed():
+func _on_restart_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
-		
-func _on_exit_button_pressed():
+
+func _on_exit_pressed() -> void:
 	get_tree().quit()
--- Scripts/WorldOne.gd
+++ Scripts/WorldOne.gd
@@ -9,5 +9,5 @@ func _on_area_2d_body_entered(body):
 	get_tree().reload_current_scene()
 
 func _on_end_map_body_entered(body):
-	if Global.enemyKilledCount >= 1:
+	if Global.enemyKilledCount >= 8:
 		get_tree().change_scene_to_packed(endScreen)
